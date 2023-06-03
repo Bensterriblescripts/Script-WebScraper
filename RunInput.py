4bdf37377d9ebdcff51299965fc2b892938e3506
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-06-03 15:24:33 +1200

    renamed scrapers to ease reporting and started a new file to reduce size

---
+++ RunInput.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+scriptfolders = ["Webscrapers - Wellington"]
+
+input("Enter the script number to run it")
+
+for folder_path in scriptfolders:
+    file_list = os.listdir(folder_path)
+    python_files = [file for file in file_list if file.endswith(".py")]
+    for file in python_files:
+        file_path = os.path.join(folder_path, file)
+        subprocess.run(["python", file_path])
